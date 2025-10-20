27eae1e8c6f1eaf5cc2c81ff01e91bc9d41f7795
Alejandro Serrano <jandoneko@MacBook-Air-de-Alejandro.local>
2025-10-20 00:20:07 -0600

    correccion en el dockerfile 4

--- server.js
+++ server.js
@@ -21,14 +21,6 @@ app.use(express.json());
 app.use('/api', downloadRoutes);
 
 
-if (process.env.NODE_ENV === 'production') {
-  const frontendPath = path.join(__dirname, '../frontend/build');
-  app.use(express.static(frontendPath));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(frontendPath, 'index.html'));
-  });
-}
 
 
 app.listen(port, () => {
